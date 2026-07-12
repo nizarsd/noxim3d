4b4ab1d70b80e344a5a3dd7268c86855dd8f6f65
nizarsd <nizarsd@example.com>
2026-07-13 01:53:33 +0300

    commit

--- gitnoxim.bash
+++ gitnoxim.bash
@@ -1,12 +1,13 @@
 # 1. Stage only .cpp and .h files across all subdirectories
 #git init
 
+CM=${1:-updates}
 git add *.cpp *.h *.bash *.txt
 
 # git remote set-url origin https://github.com/nizarsd/noxim3d.git
 
 # 2. Commit the changes
-git commit -m "Noxim oddevenBalanced fix and simulator performance improvements, sanity checked"
+git commit -m "$CM"
 
 # 3. Push to GitHub
 git push origin main
